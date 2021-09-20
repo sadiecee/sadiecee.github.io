87f3fe24f0e3fcacf6427b470e2a865f6f81d65a
SadieCee <SADMURRAY@CLARKU.EDU>
2021-09-20 00:12:05 -0700

    Completed Task 4. Operators

--- javascript/external.js
+++ javascript/external.js
@@ -11,13 +11,18 @@
 
 //A prompt box is used to prompt users to input a value
 //before entering a page.
-user_name = window.prompt("Please enter your name", "Type your name
-here");
-document.write(user_name);
+// user_name = window.prompt("Please enter your name", "Type your name
+//here");
+//document.write(user_name);
 // there are many ways to use the prompt feature
-sign = window.prompt(); // open the blank prompt window
-sign = prompt(); // open the blank prompt window
-sign = window.prompt('Are you feeling lucky'); // open the window with Text
+//sign = window.prompt(); // open the blank prompt window
+//sign = prompt(); // open the blank prompt window
+//sign = window.prompt('Are you feeling lucky'); // open the window with Text
 //"Are you feeling lucky"
-sign = window.prompt('Are you feeling lucky', 'sure'); // open the window with
+//sign = window.prompt('Are you feeling lucky', 'sure'); // open the window with
 //Text "Are you feeling lucky" and default value "sure"
+x = 22;
+const y =33;
+document.writeln(x + y) ;
+document.writeln("<br>");
+document.writeln(x += 33);
