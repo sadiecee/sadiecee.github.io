9097db71b0501da76fbf5478c303ea284a8ca47a
SadieCee <SADMURRAY@CLARKU.EDU>
2021-09-20 00:19:41 -0700

    Completed Task 6. Array

--- javascript/external.js
+++ javascript/external.js
@@ -12,13 +12,13 @@
 //A prompt box is used to prompt users to input a value
 //before entering a page.
 // user_name = window.prompt("Please enter your name", "Type your name here");
-//document.write(user_name);
-// there are many ways to use the prompt feature
-//sign = window.prompt(); // open the blank prompt window
-//sign = prompt(); // open the blank prompt window
-//sign = window.prompt('Are you feeling lucky'); // open the window with Text
+// document.write(user_name);
+//there are many ways to use the prompt feature
+// sign = window.prompt(); // open the blank prompt window
+// sign = prompt(); // open the blank prompt window
+// sign = window.prompt('Are you feeling lucky'); // open the window with Text
 //"Are you feeling lucky"
-//sign = window.prompt('Are you feeling lucky', 'sure'); // open the window with
+// sign = window.prompt('Are you feeling lucky', 'sure'); // open the window with
 //Text "Are you feeling lucky" and default value "sure"
 
 // x = 22;
@@ -27,12 +27,17 @@
 //document.writeln("<br>");
 //document.writeln(x += 33);
 
-document.writeln("<button onclick='condition()'>Conditional Test</button>")
-function condition() {
-  x = confirm("Are you sure you want to proceed?");
-  if(x) {
-    document.writeln("You chose Okay!");
-  } else {
-    document.writeln("You chose Cancel!");
-  }
+// document.writeln("<button onclick='condition()'>Conditional Test</button>")
+// function condition() {
+//   x = confirm("Are you sure you want to proceed?");
+//   if(x) {
+//     document.writeln("You chose Okay!");
+//   } else {
+//     document.writeln("You chose Cancel!");
+//   }
+// }
+
+arr = ["Joe","April","Mark","Sara"];
+for (var i=0; i < arr.length; i++) {
+  document.writeln(arr[i],"<br>");
 }
