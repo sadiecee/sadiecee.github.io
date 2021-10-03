1e915df176a74a90968b1c1e2b737db3eda6cf19
SadieCee <SADMURRAY@CLARKU.EDU>
2021-10-03 16:11:57 -0700

    Filled content from web maps array into the web maps table

--- javascript/external.js
+++ javascript/external.js
@@ -28,7 +28,32 @@ function webmap_table(){
   document.write("</table>");
 }
 
+var webmaps =
+  [
+    ["Oil Spill Toolkit",
+    "https://www.glo.texas.gov/ost/",
+    "The Oil Spill Toolkit developed by Enterprise Technology Solutions"
+    ],
+    ["Texas Ecosystems Analytical Mapper",
+    "http://tpwd.texas.gov/gis/team/",
+    "The Texas Parks and Wildlife's Landscape Ecology program developed"
+    ]
+  ];
 
+  function webmap_table()
+  {
+    document.write("<table width=100%>");
+    for (var row=0; row < webmaps.length; row++)
+    {
+      document.write("<tr>");
+      for (var column=0; column < webmaps[0].length; column++)
+      {
+        document.write("<td>" + webmaps[row][column] + "</td>");
+      }
+      document.write("</tr>");
+    }
+    document.write("</table>")
+  }
 
 
 
