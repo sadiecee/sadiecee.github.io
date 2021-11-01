7f7ca322b27944c49b147ce433464cce54c2029e
SadieCee <SADMURRAY@CLARKU.EDU>
2021-10-31 19:46:15 -0700

    fixing error messages on console; cannot find file even though path exis

--- javascript/maps.js
+++ javascript/maps.js
@@ -106,7 +106,6 @@ marker.on('click', function(e){
 });
 
 
- MAPBOX TITLE LAYER
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   maxZoom: 18,
   id: 'mapbox/satellite-streets-v11',
